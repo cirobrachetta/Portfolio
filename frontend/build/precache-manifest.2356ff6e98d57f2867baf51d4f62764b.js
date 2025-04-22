6d9d984f14c591d43536fccc540156fbccdb3f90
ciro brachetta <cirobrachetta@gmail.com>
2025-04-22 16:57:53 -0300

    V_2

---
+++ frontend/build/precache-manifest.2356ff6e98d57f2867baf51d4f62764b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ef6b1907d06409beb4cdf564b1db67",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "36758326b9271199f5d2",
+    "url": "/Portfolio/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "4e390e91657457ee4166",
+    "url": "/Portfolio/static/css/main.a2bd7709.chunk.css"
+  },
+  {
+    "revision": "36758326b9271199f5d2",
+    "url": "/Portfolio/static/js/2.7af3abcb.chunk.js"
+  },
+  {
+    "revision": "4e390e91657457ee4166",
+    "url": "/Portfolio/static/js/main.a562c700.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "b10640fe1029953d978087d563814850",
+    "url": "/Portfolio/static/media/ciro-perfil.b10640fe.jpg"
+  }
+]);
--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <p className="lead text-center">
-            Soy un apasionado Analista de Sistemas con destacada experiencia en el diseño y desarrollo de sistemas de software innovadores.
+            Soy un apasionado de los lenguajes incluido el musical. Analista de Sistemas con destacada experiencia en el diseño y desarrollo de sistemas de software innovadores.
              Mi enfoque está en ofrecer soluciones tecnológicas eficientes y escalables que impulsen el éxito empresarial.
             Estoy entusiasmado por la oportunidad de colaborar en proyectos desafiantes que requieran experiencia en diseño y desarrollo de sistemas de software. ¿Listo para llevar tus soluciones tecnológicas al siguiente nivel?
              ¡Conéctate conmigo para explorar cómo puedo aportar valor a tu equipo y organización!
