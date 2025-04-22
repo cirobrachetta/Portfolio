4f3e32565ee8435f73b661e45570ee3627c7acfc
ciro brachetta <cirobrachetta@gmail.com>
2025-04-22 16:29:18 -0300

    Updates

---
+++ precache-manifest.0064120bbb6ea1b0ca23add4730348f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d07b6e6a03ae0f3d5d38cc828660f6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "36758326b9271199f5d2",
+    "url": "/Portfolio/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "38ae41e3b411825af1d7",
+    "url": "/Portfolio/static/css/main.a2bd7709.chunk.css"
+  },
+  {
+    "revision": "36758326b9271199f5d2",
+    "url": "/Portfolio/static/js/2.7af3abcb.chunk.js"
+  },
+  {
+    "revision": "38ae41e3b411825af1d7",
+    "url": "/Portfolio/static/js/main.ee99687d.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "b10640fe1029953d978087d563814850",
+    "url": "/Portfolio/static/media/ciro-perfil.b10640fe.jpg"
+  }
+]);
